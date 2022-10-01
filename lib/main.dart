f0af3c7115ede47c595810b27dc4c67219ef3aa1
Yanisa Phetklom <phetklom_y@silpakorn.edu>
2022-10-01 22:07:09 +0700

    เพิ่มโลโก้/ธง

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Palette.myColor, // กำหนดธีมโดยใช้ชุดสี myColor ในคลาส Palette
       ),
       home: const ScoreBoxing(),
     );
   }
+}
+
+
+// เพิ่มคลาสนี้
+class Palette {
+  static const MaterialColor myColor = const MaterialColor(
+    0xff8b2030,
+    const <int, Color>{
+      50: const Color(0xff7d1d2b), //10%
+      100: const Color(0xff6f1a26), //20%
+      200: const Color(0xff611622), //30%
+      300: const Color(0xff53131d), //40%
+      400: const Color(0xff461018), //50%
+      500: const Color(0xff380d13), //60%
+      600: const Color(0xff2a0a0e), //70%
+      700: const Color(0xff1c060a), //80%
+      800: const Color(0xff0e0305), //90%
+      900: const Color(0xff000000), //100%
+    },
+  );
 }
--- lib/pages/page_score.dart
+++ lib/pages/page_score.dart
@@ -10,6 +10,51 @@ class ScoreBoxing extends StatefulWidget {
 class _ScorePageState extends State<ScoreBoxing> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(title: const Text('OLYMPIC BOXING SCORING')),
+           body: Center(
+               child: Column(
+                children: [
+                  SizedBox(height: 20.0),
+                  Image.asset('assets/images/logo.png',width: 250.0,),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: 1500.0,
+                    height: 20.0,
+                    color: Colors.black,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Women's Light (57-60kg) Semi-final",
+                          style: TextStyle(
+                            fontSize: 10.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+               Row(
+                 children: [
+                   Icon(Icons.person,
+                       color: Color(0xffb71c1c),
+                       size: 75.25),
+                     Image.asset('assets/images/flag_ireland.png',width: 35.0,),
+                 ],
+               ),
+
+
+                      Row(
+
+                      )
+
+
+                ],
+    )
+    )
+
+    );
   }
 }
