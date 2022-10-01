a6ca6712906bd3515d2fd17761003d6bb4bbb793
Yanisa Phetklom <phetklom_y@silpakorn.edu>
2022-10-01 20:19:48 +0700

    ครั้งที่1

---
+++ android/app/src/main/kotlin/com/example/hw4_olympic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hw4_olympic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/page_score.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScoreBoxing extends StatefulWidget {
+  const ScoreBoxing({Key? key}) : super(key: key);
+
+  @override
+  State<ScoreBoxing> createState() => _ScorePageState();
+}
+
+class _ScorePageState extends State<ScoreBoxing> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
